630de65c1f820cb6634e32bfab1f2359568f4149
acrinym <acrinym@gmail.com>
2026-07-31 14:12:21 -0400

    Export native SBaGenX validation API

--- pysbagen/__init__.py
+++ pysbagen/__init__.py
@@ -11,6 +11,14 @@ from .api import (
 from .compatibility import CompatibilityState, ImportReport, RenderDisposition
 from .importers import ImportedArtifact, import_artifact, import_drg, import_sbg
 from .sbagenx_backend import BackendCapability, SBaGenXProbe, probe_sbagenx
+from .sbagenx_native import (
+    NativeDiagnostic,
+    NativeValidationReport,
+    SBaGenXNative,
+    SBaGenXNativeError,
+    UnsupportedSBaGenXAPI,
+    validate_sbagenx_source,
+)
 from .sleep import SleepLayers, SleepRecipe, SleepRequest, build_sleep_recipe
 
 __all__ = [
@@ -18,12 +26,17 @@ __all__ = [
     "CompatibilityState",
     "ImportReport",
     "ImportedArtifact",
+    "NativeDiagnostic",
+    "NativeValidationReport",
     "RenderDisposition",
     "RenderResult",
+    "SBaGenXNative",
+    "SBaGenXNativeError",
     "SBaGenXProbe",
     "SleepLayers",
     "SleepRecipe",
     "SleepRequest",
+    "UnsupportedSBaGenXAPI",
     "build_sleep_recipe",
     "import_artifact",
     "import_drg",
@@ -33,6 +46,7 @@ __all__ = [
     "render_schedule",
     "render_sleep",
     "render_specs",
+    "validate_sbagenx_source",
     "write_audio",
 ]
 __version__ = "0.4.0"

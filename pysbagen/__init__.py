5065630849dfdaf18560682cb8d9e2cbb79fbce3
acrinym <acrinym@gmail.com>
2026-07-31 01:27:39 -0400

    Publish the compatibility API

--- pysbagen/__init__.py
+++ pysbagen/__init__.py
@@ -1,17 +1,34 @@
 """pysbagen public package."""
 
-from .api import RenderResult, render_schedule, render_sleep, render_specs, write_audio
+from .api import (
+    RenderResult,
+    inspect_artifact,
+    render_schedule,
+    render_sleep,
+    render_specs,
+    write_audio,
+)
+from .compatibility import CompatibilityState, ImportReport, RenderDisposition
+from .importers import ImportedArtifact, import_artifact, import_drg, import_sbg
 from .sleep import SleepLayers, SleepRecipe, SleepRequest, build_sleep_recipe
 
 __all__ = [
+    "CompatibilityState",
+    "ImportReport",
+    "ImportedArtifact",
+    "RenderDisposition",
     "RenderResult",
     "SleepLayers",
     "SleepRecipe",
     "SleepRequest",
     "build_sleep_recipe",
+    "import_artifact",
+    "import_drg",
+    "import_sbg",
+    "inspect_artifact",
     "render_schedule",
     "render_sleep",
     "render_specs",
     "write_audio",
 ]
-__version__ = "0.3.0"
+__version__ = "0.4.0"
